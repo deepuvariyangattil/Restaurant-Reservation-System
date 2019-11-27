c880890320d2a56b2e32a6d1e597b8b63df23048
deepuvariyangattil <40193000+dvmlive@users.noreply.github.com>
2019-11-27 17:24:47 -0500

    Index data initial

---
+++ data/index.js
@@ -0,0 +1,11 @@
+const reservationData = require("./reservations");
+const restaurantData = require("./restaurants");
+const managerData = require("./managerinfo");
+
+
+module.exports = {
+  reservations: reservationData,
+  restaurants: restaurantData,
+  managers: managerData,
+  
+};
